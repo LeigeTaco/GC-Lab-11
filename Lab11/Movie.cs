77b5039f186ad6bd8e263177d4591778a6e463e5
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-08-01 11:20:50 -0400

    First commit, also don't judge me

---
+++ Lab11/Movie.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+
+    class Movie
+    {
+
+        private string title;
+        private byte category;
+
+        public string Title
+        {
+
+            set { title = value; }
+            get { return title; }
+
+        }
+
+        public byte Category
+        {
+
+            set { category = value; }
+            get { return category; }
+
+        }
+
+        public string CategoryToString()
+        {
+
+            switch (category)
+            {
+
+                case 0:
+                    return "Adam Sandler";
+                case 1:
+                    return "Action";
+                case 2:
+                    return "Comedy";
+                case 3:
+                    return "Romance";
+                case 4:
+                    return "Horror";
+                case 5:
+                    return "Documentary";
+                case 6:
+                    return "Animated";
+                case 7:
+                    return "Adventure";
+                case 8:
+                    return "Thriller";
+                case 9:
+                    return "Crime";
+                case 10:
+                    return "Western";
+                case 11:
+                    return "Musical";
+                case 12:
+                    return "Noir";
+                case 13:
+                    return "Sports";
+                case 14:
+                    return "Mystery";
+                case 15:
+                    return "Sci-fi";
+                default:
+                    return "Adam Sandler";
+
+            }
+
+        }
+
+        public void StringToCategory(Dictionary<string, byte> refernce, string cat)
+        {
+
+            try
+            {
+
+                Console.WriteLine($"Genre found: {cat} ID {reference[cat]}");
+                Category = reference[cat.ToLower()];
+
+            }
+            catch
+            {
+
+                Console.WriteLine("We could not find that genre, defaulting to \"Adam Sandler\"");
+                Category = 0;
+
+            }
+
+        }
+
+        public Movie()
+        {
+
+            Title = "The Cobbler";
+            Category = 0;
+
+        }
+
+        public Movie(string title, byte cat)
+        {
+
+            Title = title;
+            Category = cat;
+
+        }
+
+    }
+
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+
+    class Program
+    {
+
+        //static 
+
+        static void Main(string[] args)
+        {
+
+            #region Data Storage
+            Dictionary<string, byte> reference = new Dictionary<string, byte>();
+            reference.Add("adam sandler", 0);
+            reference.Add("action", 1);
+            reference.Add("comedy", 2);
+            reference.Add("romance", 3);
+            reference.Add("horror", 4);
+            reference.Add("documentary", 5);
+            reference.Add("animated", 6);
+            reference.Add("adventure", 7);
+            reference.Add("thriller", 8);
+            reference.Add("crime", 9);
+            reference.Add("western", 10);
+            reference.Add("musical", 11);
+            reference.Add("noir", 12);
+            reference.Add("sports", 13);
+            reference.Add("mystery", 14);
+            reference.Add("scifi", 15);
+
+            Dictionary<byte, List<Movie>> lookup = new Dictionary<byte, List<Movie>>();
+            lookup.Add(0, new List<Movie>());       //Adam Sandler
+            lookup.Add(1, new List<Movie>());       //Action
+            lookup.Add(2, new List<Movie>());       //Comedy
+            lookup.Add(3, new List<Movie>());       //Romance
+            lookup.Add(4, new List<Movie>());       //Horror
+            lookup.Add(5, new List<Movie>());       //Documentary
+            lookup.Add(6, new List<Movie>());       //Animated
+            lookup.Add(7, new List<Movie>());       //Adventure
+            lookup.Add(8, new List<Movie>());       //Thriller
+            lookup.Add(9, new List<Movie>());       //Crime
+            lookup.Add(10, new List<Movie>());      //Western
+            lookup.Add(11, new List<Movie>());      //Musical
+            lookup.Add(12, new List<Movie>());      //Noir
+            lookup.Add(13, new List<Movie>());      //Sports
+            lookup.Add(14, new List<Movie>());      //Mystery
+            lookup.Add(15, new List<Movie>());      //Sci-fi
+
+            #endregion
+
+        }
+
+    }
+
+}
